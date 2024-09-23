c31ce949c350730fd745ed198f64ea7f001123b9
aquestry <155507142+aquestry@users.noreply.github.com>
2024-09-23 13:46:07 +0200

    Add

--- src/main/java/de/voasis/serverHandlerProxy/ServerHandlerProxy.java
+++ src/main/java/de/voasis/serverHandlerProxy/ServerHandlerProxy.java
@@ -4,16 +4,62 @@ import com.google.inject.Inject;
 import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
 import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.plugin.Plugin;
+import com.velocitypowered.api.plugin.PluginContainer;
+import com.velocitypowered.api.plugin.annotation.DataDirectory;
+import com.velocitypowered.api.proxy.ProxyServer;
+import dev.dejvokep.boostedyaml.YamlDocument;
+import dev.dejvokep.boostedyaml.dvs.versioning.BasicVersioning;
+import dev.dejvokep.boostedyaml.settings.dumper.DumperSettings;
+import dev.dejvokep.boostedyaml.settings.general.GeneralSettings;
+import dev.dejvokep.boostedyaml.settings.loader.LoaderSettings;
+import dev.dejvokep.boostedyaml.settings.updater.UpdaterSettings;
 import org.slf4j.Logger;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Objects;
+import java.util.Optional;
 
-@Plugin(id = "serverhandlerproxy", name = "ServerHandlerProxy", version = "1.0-SNAPSHOT", authors = "Aquestry")
+@Plugin(id = "serverhandlerproxy", name = "ServerHandlerProxy", version = "1.0", authors = "Aquestry")
 public class ServerHandlerProxy {
-
     @Inject
     private Logger logger;
+    @Inject
+    private ProxyServer server;
 
+    private static YamlDocument config;
+
+    @Inject
+    public ServerHandlerProxy(ProxyServer server, Logger logger, @DataDirectory Path dataDirectory) {
+        try {
+            config = YamlDocument.create(
+                    new File(dataDirectory.toFile(), "config.yml"),
+                    Objects.requireNonNull(
+                            getClass().getResourceAsStream("/config.yml")
+                    ),
+                    GeneralSettings.DEFAULT,
+                    LoaderSettings.builder()
+                            .setAutoUpdate(true)
+                            .build(),
+                    DumperSettings.DEFAULT,
+                    UpdaterSettings.builder()
+                            .setVersioning(new BasicVersioning("file-version"))
+                            .setOptionSorting(UpdaterSettings.OptionSorting.SORT_BY_DEFAULTS)
+                            .build()
+            );
+            config.update();
+            config.save();
+        } catch (IOException e) {
+            logger.error("Could not load config! Plugin will shutdown!");
+            Optional<PluginContainer> container = server.getPluginManager().getPlugin("serverhandlerproxy");
+            container.ifPresent(pluginContainer -> pluginContainer.getExecutorService().shutdown());
+        }
+    }
     @Subscribe
     public void onProxyInitialization(ProxyInitializeEvent event) {
-
+        logger.info("ServerHandlerProxy started");
+        logger.info("Servers:");
+        logger.info(server.getAllServers().toString());
+        logger.info("Default Server: {}", config.getString("defaultServer"));
     }
 }
