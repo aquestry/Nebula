d6afef8e46168fcdf4bdc9ddb68446dc2e4380f6
aquestry <155507142+aquestry@users.noreply.github.com>
2024-11-08 18:50:14 +0100

    State: Working This update did: - Should all work now - Cleanup

--- src/main/java/de/voasis/nebula/Event/Events/Login.java
+++ src/main/java/de/voasis/nebula/Event/Events/Login.java
@@ -7,14 +7,20 @@ import de.voasis.nebula.Data.Data;
 import de.voasis.nebula.Nebula;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import java.util.UUID;
 
 public class Login {
     private final Logger logger = LoggerFactory.getLogger("nebula");
+
     public Login(LoginEvent event, ProxyServer server) {
         Player player = event.getPlayer();
-        if (Data.adminUUIDs.contains(player.getUniqueId().toString())) {
+        UUID playerUUID = player.getUniqueId();
+        if (Data.adminUUIDs.contains(playerUUID.toString())) {
             Nebula.permissionManager.addPermission(player, "velocity.*");
         }
-        logger.info("User logged in: {}, UUID: {}, is admin : {}.", player.getUsername(),player.getUniqueId().toString(), player.hasPermission("velocity.admin"));
+        logger.info("User logged in: {}, UUID: {}, is admin : {}.",
+                player.getUsername(), playerUUID,
+                player.hasPermission("velocity.admin"));
     }
+
 }
