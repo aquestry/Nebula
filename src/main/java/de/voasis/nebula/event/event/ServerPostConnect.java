566482f342d367812b61a186b7e41231e70747d5
aquestry <155507142+aquestry@users.noreply.github.com>
2024-12-25 23:57:40 +0100

    Update

--- src/main/java/de/voasis/nebula/event/event/ServerPostConnect.java
+++ src/main/java/de/voasis/nebula/event/event/ServerPostConnect.java
@@ -13,14 +13,11 @@ public class ServerPostConnect {
         server.sendPluginMessage(Nebula.channelMain, Nebula.permissionManager.getGroupInfo(player).getBytes());
         String rankName = Nebula.permissionManager.getGroup(player).getPrefix();
         String serverName = server.getServerInfo().getName().split("-")[0];
-        String hubName = Nebula.util.getBackendServer(server.getServerInfo().getName())
-                .getHoldServer()
-                .getServerName();
+        String hubName = Nebula.util.getBackendServer(server.getServerInfo().getName()).getHoldServer().getServerName();
         hubName = hubName.substring(0, 1).toUpperCase() + hubName.substring(1);
         serverName = serverName.substring(0, 1).toUpperCase() + serverName.substring(1);
         String score = player.getUsername() + "&<blue><bold>Nebula</bold></blue>&<reset>#<white>Rank: " + rankName + "#<white>Service: " + serverName + "#<white>Hub: " + hubName + "#<reset>";
         server.sendPluginMessage(Nebula.channelScore, score.getBytes());
-        player.sendPlayerListHeaderAndFooter(Nebula.mm.deserialize("<blue><bold>Nebula</bold></blue>\nPlayers on network: " + Nebula.server.getAllPlayers().size() + "/" + Nebula.server.getConfiguration().getShowMaxPlayers())
-                ,Nebula.mm.deserialize("Your ping is: " + player.getPing() + "ms"));
+        player.sendPlayerListHeader(Nebula.mm.deserialize("<blue><bold>Nebula Network</bold></blue>"));
     }
 }
